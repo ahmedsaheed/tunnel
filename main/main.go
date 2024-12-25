ab610cbafd894422e73d5d289467cda8c46b81a2
ahmedsaheed <ahmedsaheed2@outlook.com>
2024-12-25 00:19:14 +0000

    feat: port flags as args

--- main/main.go
+++ main/main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
+	"strconv"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8080")
+
+	// tunnel --from <target_port> --to <server_port>
+	from := flag.Int("from", 3000, "Port to forward to")
+	to := flag.Int("to", 8080, "Port to listen on")
+	flag.Parse()
+	fmt.Println(flag.Args())
+
+	if len(os.Args[1:]) == 0 {
+		log.Println("No flags provided, using default ports")
+		log.Printf("tunnel --from %d --to %d", *from, *to)
+	}
+	if *from == 0 || *to == 0 {
+		log.Println("Please provide the port to forward and the port to listen on")
+		log.Fatalf("Usage: tunnel --from <target_port> --to <server_port>")
+	}
+
+	serverPort := strconv.Itoa(*to)
+	targetPort := strconv.Itoa(*from)
+	listener, err := net.Listen("tcp", "localhost:"+serverPort)
+
 	if err != nil {
 		log.Fatalf("Error starting listener: %v", err)
 	}
 	defer listener.Close()
 
-	log.Println("Tunnel is running: localhost:8080 -> localhost:3000")
-
+	log.Println("Tunnel is running: localhost:" + serverPort + "-> localhost:" + targetPort)
 	for {
 		clientConn, err := listener.Accept()
 		if err != nil {
@@ -22,15 +44,15 @@ func main() {
 			continue
 		}
 
-		go handleConnection(clientConn)
+		go handleConnection(clientConn, targetPort)
 	}
 }
 
-func handleConnection(clientConn net.Conn) {
+func handleConnection(clientConn net.Conn, targetPort string) {
 	defer clientConn.Close()
 
 	// Connect to the target server on port 4000
-	targetConn, err := net.Dial("tcp", "localhost:3000")
+	targetConn, err := net.Dial("tcp", "localhost:"+targetPort)
 	if err != nil {
 		log.Printf("Error connecting to target: %v", err)
 		return
